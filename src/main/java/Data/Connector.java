dc4cab0b403980156398e68597c853f1dbf7e445
cph-ms782 <cph-ms782@cphbusiness.dk>
2019-03-23 15:21:16 +0100

    connection_closing_and_opening

--- src/main/java/Data/Connector.java
+++ src/main/java/Data/Connector.java
@@ -1,5 +1,6 @@
 package Data;
 
+import Logic.Exceptions.LoginSampleException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,9 +8,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * The purpose of Connector is to...
+ * Database connector. Will open up closed connection Is not a singelton at the
+ * moment
  *
- * @author kasper
+ * @author martin
  */
 public class Connector
 {
@@ -19,21 +21,46 @@ public class Connector
     private static final String USERNAME = "testuser";
     private static final String PASSWORD = "password123";
 
-    public static Connection connection(Connection con) throws ClassNotFoundException, SQLException
+    /**
+     * open connection if there is none or reopen if its closed
+     * 
+     * @param con
+     * @return
+     * @throws LoginSampleException (SQL exceptions and classnotfoundException
+     */
+    public static Connection connection(Connection con) throws LoginSampleException
     {
-        if (con != null && !con.isClosed())
+        try
         {
-            return con;
-        }
+            if (con != null && !con.isClosed())//sqlexception
+            {
+                return con;
+            }
 
-        if (con == null || con.isClosed())
+            if (con == null || con.isClosed())//sqlexception
+            {
+                Class.forName(DRIVER); //class not found exception
+                con = DriverManager.getConnection(URL, USERNAME, PASSWORD); //sqlexception
+            }
+            return con;
+        } catch (SQLException ex)
         {
-            Class.forName(DRIVER);
-            con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            throw new LoginSampleException("Der er sket en fejl i forbindelsen med databasen (Connector.connection): "
+                    + ex.getMessage());
+        } catch (ClassNotFoundException ex)
+        {
+            throw new LoginSampleException("Der er ikke fundet en driver til forbindelsen til databasen (Connector.connection): "
+                    + ex.getMessage());
         }
-        return con;
     }
 
+    /**
+     * Closing connections to database, so user will be able to use database after some time has gone
+     * 
+     * @param rs
+     * @param ps
+     * @param con 
+     */
     public static void CloseConnection(ResultSet rs, PreparedStatement ps, Connection con)
     {
         try
